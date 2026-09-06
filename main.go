9b45a149ecaeacfc07fe8906b51917760309a2ec
Your Name <you@example.com>
2026-09-06 22:30:15 +0900

    use cache instead of DB querying for postIsuCondition

--- main.go
+++ main.go
@@ -1480,20 +1480,10 @@ func postIsuCondition(c echo.Context) error {
 		return c.String(http.StatusBadRequest, "bad request body")
 	}
 
-	tx, err := db.Beginx()
-	if err != nil {
-		c.Logger().Errorf("db error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	defer tx.Rollback()
-
-	var count int
-	err = tx.Get(&count, "SELECT COUNT(*) FROM `isu` WHERE `jia_isu_uuid` = ?", jiaIsuUUID)
-	if err != nil {
-		c.Logger().Errorf("db error: %v", err)
-		return c.NoContent(http.StatusInternalServerError)
-	}
-	if count == 0 {
+	isuMetaCache.mu.RLock()
+	_, ok := isuMetaCache.m[jiaIsuUUID]
+	isuMetaCache.mu.RUnlock()
+	if !ok {
 		return c.String(http.StatusNotFound, "not found: isu")
 	}
 
@@ -1519,6 +1509,13 @@ func postIsuCondition(c echo.Context) error {
 			Message:    cond.Message,
 		})
 	}
+
+	tx, err := db.Beginx()
+	if err != nil {
+		c.Logger().Errorf("db error: %v", err)
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	defer tx.Rollback()
 
 	// bulk insertする
 	_, err = tx.NamedExec(

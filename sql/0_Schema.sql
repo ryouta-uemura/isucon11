988fb7b6e80696801729c51d1c0055ad9716740f
Ryota Uemura <ryota.uemura.engineer@gmail.com>
2026-09-09 12:20:01 +0900

    add index to character (score:67000)

--- sql/0_Schema.sql
+++ sql/0_Schema.sql
@@ -15,6 +15,8 @@ CREATE TABLE `isu` (
    PRIMARY KEY(`id`)
 ) ENGINE=InnoDB DEFAULT CHARACTER SET=utf8mb4;
 
+ALTER TABLE isu ADD INDEX character_idx (`character`);
+
 CREATE TABLE `isu_condition` (
   `id` bigint AUTO_INCREMENT,
   `jia_isu_uuid` CHAR(36) NOT NULL,
